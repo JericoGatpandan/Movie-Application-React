7c13dbb325500bd735d9b839d401854fe49b5d37
Jerico Gatpandan <jericogatpandan0905@gmail.com>
2025-08-10 16:29:22 +0800

    fixed another bug

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/movie-app-react/',
+  base: '/Movie-Application-React/',
   plugins: [react(), tailwindcss()],
 })
